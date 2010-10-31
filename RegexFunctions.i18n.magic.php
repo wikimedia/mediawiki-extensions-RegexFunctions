b0cedd62f428f26d24772b302c71847943d2c625
Siebrand Mazeland <siebrand@users.mediawiki.org>
2010-10-31 17:55:47 +0000

    Localisation updates from translatewiki.net

--- RegexFunctions.i18n.magic.php
+++ RegexFunctions.i18n.magic.php
@@ -2,8 +2,16 @@
 
 $magicWords = array();
 
+/** English (English) */
 $magicWords['en'] = array(
-	'rmatch'   => array( 0, 'rmatch' ),
-	'rsplit'   => array( 0, 'rsplit' ),
+	'rmatch' => array( 0, 'rmatch' ),
+	'rsplit' => array( 0, 'rsplit' ),
 	'rreplace' => array( 0, 'rreplace' ),
+);
+
+/** Japanese (日本語) */
+$magicWords['ja'] = array(
+	'rmatch' => array( 0, '正規表現一致' ),
+	'rsplit' => array( 0, '正規表現分割' ),
+	'rreplace' => array( 0, '正規表現置き換え' ),
 );
