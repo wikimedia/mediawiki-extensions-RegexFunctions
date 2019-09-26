7be898ddfc9141659a7f32a0dc3e24bbc8cda224
Jack Phoenix <ashley@uncyclomedia.co>
2019-09-26 14:25:01 +0300

    Mandatory extension registration, requires now MW 1.29+
    Inspired by the HydraWiki fork at https://gitlab.com/hydrawiki/extensions/regexfunctions , I just tweaked a few things (main PHP class name, config var descriptions).
    
    Change-Id: Ibee58d2abc4940ef7666bfc3c0a875171dcda8cd

--- RegexFunctions.php
+++ RegexFunctions.php
@@ -9,37 +9,16 @@
  * @link https://www.mediawiki.org/wiki/Extension:RegexFunctions Documentation
  */
 
-if ( !defined( 'MEDIAWIKI' ) ) {
-	echo "This file is an extension of the MediaWiki software and cannot be used standalone\n";
-	die( 1 );
+if ( function_exists( 'wfLoadExtension' ) ) {
+	wfLoadExtension( 'RegexFunctions' );
+	// Keep i18n globals so mergeMessageFileList.php doesn't break
+	$wgMessagesDirs['RegexFunctions'] = __DIR__ . '/i18n';
+	wfWarn(
+		'Deprecated PHP entry point used for RegexFunctions extension. ' .
+		'Please use wfLoadExtension instead, ' .
+		'see https://www.mediawiki.org/wiki/Extension_registration for more details.'
+	);
+	return;
+} else {
+	die( 'This version of the RegexFunctions extension requires MediaWiki 1.29+' );
 }
-
-// Extension credits that will show up on Special:Version
-$wgExtensionCredits['parserhook'][] = [
-	'path' => __FILE__,
-	'name' => 'RegexFunctions',
-	'author' => 'Ryan Schmidt',
-	'version' => '1.5.0',
-	'descriptionmsg' => 'regexfunctions-desc',
-	'url' => 'https://www.mediawiki.org/wiki/Extension:RegexFunctions',
-];
-
-$wgMessagesDirs['RegexFunctions'] = __DIR__ . '/i18n';
-$wgExtensionMessagesFiles['RegexFunctionsMagic'] = __DIR__ . '/RegexFunctions.i18n.magic.php';
-
-$wgAutoloadClasses['ExtRegexFunctions'] = __DIR__ . '/ExtRegexFunctions.php';
-
-$wgHooks['ParserFirstCallInit'][] = 'ExtRegexFunctions::onParserFirstCallInit';
-$wgHooks['ParserClearState'][] = 'ExtRegexFunctions::onParserClearState';
-
-// default globals
-// how many functions are allowed in a single page? Keep this at least above 3 for usability
-$wgRegexFunctionsPerPage = 10;
-// should we allow modifiers in the functions, e.g. the /i modifier for case-insensitive?
-$wgRegexFunctionsAllowModifiers = true;
-// should we allow internal options to be set (e.g. (?opts) or (?opts:some regex))
-$wgRegexFunctionsAllowOptions = true;
-// limit for rsplit and rreplace functions. -1 is unlimited
-$wgRegexFunctionsLimit = -1;
-// array of functions to disable, aka these functions cannot be used :)
-$wgRegexFunctionsDisable = [];
